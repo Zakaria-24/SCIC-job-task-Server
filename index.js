69bc48bacb2578bd5f964831c5e0fac2d8be9380
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-08-18 03:05:46 +0600

    pagination update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const corsOptions = {
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        // 'https://scic-job-task-3f800.web.app'
+        'https://radiant-strudel-258d0a.netlify.app',
     ],
     credentials: true,
     optionSuccessStatus: 200,
@@ -32,85 +32,13 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const productsCollection = client.db("scic-products").collection("products");
-
-        // get all products
-        // app.get("/products", async (req, res) => {
-
-        //     const { page = 1, search = "", sort = "" } = req.query;
-        //     const limit = 40;
-        //     const skip = (page - 1) * limit;
-
-        //     let query = {};
-        //     if (search) {
-        //         query = { brand_name : { $regex: search, $options: 'i' }}; 
-        //     }
-        //     // if (category_name) {
-        //     //     query = category_name;
-        //     // }
-        //     // if (brand) {
-        //     //     query = brand_name;
-        //     // }
-
-        //     let sortOption = {};
-        //     if (sort === "priceLowToHigh") {
-        //         sortOption.price = 1;
-        //     } else if (sort === "priceHighToLow") {
-        //         sortOption.price = -1;
-        //     } else if (sort === "newest") {
-        //         sortOption.createdAt = -1;
-        //     }
-
-        //     const total = await productsCollection.countDocuments(query);
-        //     const products = await productsCollection.find(query)
-        //     .sort(sortOption)
-        //     .skip(skip)
-        //     .limit(limit)
-        //     .toArray();
-        //     // 
-            
-        //     res.send({ products, total });
-        // });
-
-
- // get all products
-        // app.get("/products", async (req, res) => {
-
-        //     const { page = 1, search = "", sort = "" } = req.query;
-        //     const limit = 9;
-        //     const skip = (page - 1) * limit;
-
-        //     let query = {};
-        //     if (search) {
-        //         query = { brand_name: { $regex: search, $options: 'i' } };
-        //     }
-        //     // if (category_name) {
-        //     //     query = category_name;
-        //     // }
-        //     // if (brand) {
-        //     //     query = brand_name;
-        //     // }
-
-        //     let sortOption = {};
-        //     if (sort === "priceLowToHigh") {
-        //         sortOption.price = 1;
-        //     } else if (sort === "priceHighToLow") {
-        //         sortOption.price = -1;
-        //     } else if (sort === "newest") {
-        //         sortOption.createdAt = -1;
-        //     }
-
-        //     // const total = await productsCollection.countDocuments(query);
-        //     const products = await productsCollection.find(query).skip(skip).limit(limit).sort(sortOption).toArray();
-        //     // 
-
-        //     res.send({ products });
-        // });
-
+        
         app.get('/products', async (req, res) => {
             try {
                 // Extract query parameters
-                const { search, category, brand_name, minPrice, maxPrice, sortBy, sortOrder } = req.query;
-
+                const { page=1, search, category, brand_name, minPrice, maxPrice, sortBy, sortOrder } = req.query;
+                const limit = 9;
+                const skip = (page - 1) * limit;
                 // Build the query object
                 const query = {};
 
@@ -147,7 +75,7 @@ async function run() {
                 }
 
                 // Fetch the results from the database
-                const results = await productsCollection.find(query).sort(sortOptions).toArray();
+                const results = await productsCollection.find(query).sort(sortOptions).skip(skip).limit(limit).toArray();
 
                 res.json(results);
 
